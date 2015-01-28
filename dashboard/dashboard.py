86a70b194798aea8312f76492dc719a1693d169d
Arx Cruz <apcruz@br.ibm.com>
2015-01-28 12:23:12 +0100

    Create list users page

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -3,7 +3,9 @@ from flask import render_template
 from flask import request
 
 import stackquery.common as common
+from database import db_session
 from models import Team
+from models import User
 
 dashboard = Blueprint('dashboard', __name__)
 
@@ -25,3 +27,16 @@ def dashboard_index():
 def dashboard_teams():
     teams = Team.query.all()
     return render_template('list_teams.html', teams=teams)
+
+
+@dashboard.route('/users/', methods=['GET', 'POST'])
+def dashboard_users():
+    if request.method == 'POST':
+        user_name = request.form.get('user_name')
+        user = User()
+        user.name = user_name
+        db_session.add(user)
+        db_session.commit()
+
+    users = User.query.all()
+    return render_template('list_users.html', users=users)
