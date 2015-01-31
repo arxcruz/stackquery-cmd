624b8d64e4459411c6bc01325835b9edc5457abe
Arx Cruz <apcruz@br.ibm.com>
2015-01-31 16:19:19 +0100

    Create user form. Still a WIP

---
+++ dashboard/forms.py
@@ -0,0 +1,10 @@
+from wtforms import BooleanField
+from wtforms import Form
+from wtforms import TextField
+from wtforms import validators
+
+class UserForm(Form):
+	name = TextField('Name', [validators.Length(min=3, max=128)])
+	user_id = TextField('User id', [validators.Length(min=4, max=25)])
+	email = TextField('Email Address', [validators.Length(min=6, max=35)])
+
