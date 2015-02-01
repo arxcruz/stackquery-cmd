09a85aa57468ff39e605a9a8d0fcd408c47bb0a8
Arx Cruz <apcruz@br.ibm.com>
2015-02-01 18:12:09 +0100

    Lot of things

--- dashboard/database.py
+++ dashboard/database.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from flask.ext.jsontools import JsonSerializableBase
 
 engine = create_engine('sqlite:///dashboard.db', convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
